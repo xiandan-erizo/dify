e6978189d3c04cd3ffea2007000cc7d2631017c5
xiandan <xiandan.erizo@gmail.com>
2025-02-24 18:23:05 +0800

    refactor(agent): 更新 prompt 模板
    - 备份原有的 ENGLISH_REACT_COMPLETION_PROMPT_TEMPLATES 和 ENGLISH_REACT_CHAT_PROMPT_TEMPLATES
    - 添加新的 prompt 模板，增加了工具使用说明和 JSON blob 格式示例
    - 新模板提供了更详细的指引，以帮助 AI 更好地理解和响应人类请求

--- api/core/agent/prompt/template.py
+++ api/core/agent/prompt/template.py
@@ -1,4 +1,4 @@
-ENGLISH_REACT_COMPLETION_PROMPT_TEMPLATES = """Respond to the human as helpfully and accurately as possible. 
+ENGLISH_REACT_COMPLETION_PROMPT_TEMPLATES_bak = """Respond to the human as helpfully and accurately as possible. 
 
 {{instruction}}
 
@@ -43,11 +43,57 @@ Question: {{query}}
 {{agent_scratchpad}}
 Thought:"""  # noqa: E501
 
+ENGLISH_REACT_COMPLETION_PROMPT_TEMPLATES="""
+尽可能帮助和准确地回应人类。
+
+{{instruction}}
+
+您可以使用以下工具:
+
+｛｛tools｝｝
+
+使用json blob通过提供动作键（tool name）和action_input键（tool input）来指定工具。
+有效的"action"值:"Final Answer"或{{tool_names}}
+
+每个$JSON_BLOB只提供一个操作，如图所示:
+
+```
+{
+"action":$TOOL_NAME，
+"action_input":$ACTION_INPUT
+}
+```
+
+遵循以下格式:
+
+Question:输入要回答的问题
+Thought:考虑前面和后面的步骤
+Action:
+```
+$JSON_BLOB
+```
+Observation:行动结果
+…（重复思考/行动/观察N次）
+Thought:我知道该怎么回答
+Action:
+```
+{
+"action":"Final Answer"，
+"action_input":"Final response to human"
+}
+```
+
+开始！提醒始终使用单个操作的有效json blob进行响应。必要时使用工具。如果合适，直接回应。格式是操作:```$JSON_BLOB```，然后是Observation:。
+{{historic_messages}}
+问题:｛｛query｝｝
+{{agent_scratchpad}}
+Thought:""
+"""  # noqa: E501
 
 ENGLISH_REACT_COMPLETION_AGENT_SCRATCHPAD_TEMPLATES = """Observation: {{observation}}
 Thought:"""
 
-ENGLISH_REACT_CHAT_PROMPT_TEMPLATES = """Respond to the human as helpfully and accurately as possible. 
+ENGLISH_REACT_CHAT_PROMPT_TEMPLATES_bak = """Respond to the human as helpfully and accurately as possible. 
 
 {{instruction}}
 
@@ -88,6 +134,48 @@ Action:
 
 Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation:.
 """  # noqa: E501
+
+ENGLISH_REACT_CHAT_PROMPT_TEMPLATES="""尽可能帮助和准确地回应人类。
+
+{{instruction}}
+
+您可以使用以下工具:
+
+｛｛tools｝｝
+
+使用json blob通过提供动作键（tool name）和action_input键（tool input）来指定工具。
+有效的"action"值:"Final Answer"或{{tool_names}}
+
+每个$JSON_BLOB只提供一个操作，如图所示:
+
+```
+{
+"action":$TOOL_NAME，
+"action_input":$ACTION_INPUT
+}
+```
+
+遵循以下格式:
+
+Question:输入要回答的问题
+Thought:考虑前面和后面的步骤
+Action:
+```
+$JSON_BLOB
+```
+Observation:行动结果
+…（重复思考/行动/观察N次）
+Thought:我知道该怎么回答
+Action:
+```
+{
+"action":"最终答案"，
+"action_input":"对人类的最终响应"
+}
+```
+
+开始！提醒始终使用单个操作的有效json blob进行响应。必要时使用工具。如果合适，直接回应。格式是操作:```$JSON_BLOB``，然后是Observation:。
+"""
 
 
 ENGLISH_REACT_CHAT_AGENT_SCRATCHPAD_TEMPLATES = ""
